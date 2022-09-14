62c63122dbe3f07e3b7f4428e1accf8271fca74c
kuliahin.a <kuliahin.a@abcloudz.com>
2022-09-14 13:50:32 +0300

    Update Base URL

--- app/src/main/java/com/ivorysoftinc/kotlin/example/data/Consts.kt
+++ app/src/main/java/com/ivorysoftinc/kotlin/example/data/Consts.kt
@@ -1,4 +1,4 @@
 package com.ivorysoftinc.kotlin.example.data
 
 const val EMPTY = ""
-const val BASE_URL = "https://TODO"
+const val BASE_URL = "https://raw.githubusercontent.com/ivorysoftinc/kotlin-example/master/list_data.json"
